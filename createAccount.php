635a642da5ebddf868f306f18379fc54c41ea0e5
Eldin Kecap <eldinkecap@gmail.com>
2022-07-08 22:23:33 +0200

    started working on create account back end

--- createAccount.php
+++ createAccount.php
@@ -2,19 +2,19 @@
     <?php
     include 'includes/header.php';
     ?>
-    <form action="" method="POST" id="createNewAccountForm">
+    <form action="includes/createAccount-inc.php" method="POST" id="createNewAccountForm">
         <div class = "formTitle">Create account</div>
 
-        <input type="text" id="username" placeholder="Enter username">  
+        <input type="text" id="username" name = 'usernameCreateAccount' placeholder="Enter username">  
         <div id="errorUsername"></div>
 
-        <input type="text" id="password" placeholder="Enter password">
+        <input type="text" id="password" name = 'passwordCreateAccount' placeholder="Enter password">
         <div id="errorPassword"></div>
 
-        <input type="text" id="email" placeholder="Enter email">
+        <input type="text" id="email" name = 'email' placeholder="Enter email">
         <div id="errorEmail"></div>
 
-        <input id="submit" type="submit">
+        <input id="submit" type="submit" name='submitCreateAccount'>
 
         <a href = "login.php" class = "createNewAccount">Back</a>
     </form>
--- includes/createAccount-inc.php
+++ includes/createAccount-inc.php
@@ -1,5 +1,11 @@
 <?php
-    require 'databases.php';
+    if(isset($_POST['submitCreateAccount'])){
+        require 'databases.php';
+        //PROVJERI DA LI MORAS IMENOVAT U DRUGIM FILES TEXT INPUTE UNIQUE IL SE POST RESETUJE SVAKI PUT KAD SE POZOVE
+        echo $_POST['usernameCreateAccount'];
+        echo "WTF";
+        print_r($_POST);
+    }
 
-    
+
 ?>
