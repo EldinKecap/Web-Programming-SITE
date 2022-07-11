6b31ec5c99d0952c8769a89207bf7b933700157c
Eldin Kecap <eldinkecap@gmail.com>
2022-07-11 08:38:47 +0200

    working on create account tab

--- includes/createAccount-inc.php
+++ includes/createAccount-inc.php
@@ -4,16 +4,22 @@
         $username = $_POST['usernameCreateAccount'];
         $password = $_POST['passwordCreateAccount'];
         $email = $_POST['email'];
-        $sql = 'INSERT INTO users (username,password,email) VALUES (?,?,?)';
+        $sql = "INSERT INTO users (username,password,email) VALUES (?,?,?)";
         $stmt = mysqli_stmt_init($conn);
+        // print_r($stmt);
+        // echo !mysqli_stmt_prepare($stmt,$sql);
         //PROVJERI DA LI MORAS IMENOVAT U DRUGIM FILES TEXT INPUTE UNIQUE IL SE POST RESETUJE SVAKI PUT KAD SE POZOVE
-        if(mysqli_stmt_prepare($stmt,$sql)){
+        if(!mysqli_stmt_prepare($stmt,$sql)){
             header("Location: ../createAccount.php?error=sqlerror");
             exit();
+        }else{
+
+//   so far so good
+            // echo $_POST['usernameCreateAccount'];
+            // echo "WTF";
+            // print_r($_POST);  
         }
-        echo $_POST['usernameCreateAccount'];
-        echo "WTF";
-        print_r($_POST);
+       
     }
 
 
