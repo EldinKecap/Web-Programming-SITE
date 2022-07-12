7ab16f5a46f7d17f9ffa35760e9eca0801e5508a
Eldin Kecap <eldinkecap@gmail.com>
2022-07-12 23:31:27 +0200

    fully implemented log out button

--- loginOut.js
+++ loginOut.js
@@ -2,10 +2,14 @@ console.log(window.location.href);
 let loggedin = window.localStorage.getItem('logStatus');
 if (window.location.href === 'http://localhost/PROJEKAT/index.php?success=loggedin') {
      loggedin = true;
+}else if(window.location.href === 'http://localhost/PROJEKAT/index.php?loggedout=true'){
+    loggedin = false;
 }
 if (loggedin) {
     document.getElementById('loginOut').textContent = 'Log out';
-    document.getElementById('loginOut').href = 'logout.php';
-
+    document.getElementById('loginOut').href = 'includes/logout.php';
+}else {
+    document.getElementById('loginOut').textContent = 'Log in';
+    document.getElementById('loginOut').href = 'login.php';
 }
 window.localStorage.setItem('logStatus',loggedin);
